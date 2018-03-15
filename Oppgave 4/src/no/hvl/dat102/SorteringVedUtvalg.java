d63c02968b7a33b1346aa227a70502367718637e
orjan <orjan@192.168.20.5>
2018-03-15 18:17:37 +0100

    sorting rip

--- Oppgave 4/src/no/hvl/dat102/SorteringVedUtvalg.java
+++ Oppgave 4/src/no/hvl/dat102/SorteringVedUtvalg.java
@@ -1,5 +1,24 @@
 package no.hvl.dat102;
 
 public class SorteringVedUtvalg {
+	public static <T extends Comparable<T>> void utvalgsSortering(T[] tab) {
+		int min;
 
+		for (int index = 0; index < tab.length - 1; index++) {
+			min = index;
+			for (int scan = index - 1; scan < tab.length; scan++) {
+				if (tab[scan].compareTo(tab[min]) < 0) {
+					min = scan;
+
+					bytterOm(tab, min, index);
+				}
+			}
+		}
+	}
+
+	private static <T extends Comparable<T>> void bytterOm(T[] tab, int index1, int index2) {
+		T temp = tab[index1];
+		tab[index1] = tab[index2];
+		tab[index2] = temp;
+	}
 }
