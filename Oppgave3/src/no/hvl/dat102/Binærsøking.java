d0f4788e763ebca37c929e73146eb29e989da76c
orjan <orjan@158-37-224-169.hib.no>
2018-03-15 17:19:10 +0100

    mr tryhard yes

--- Oppgave3/src/no/hvl/dat102/Binærsøking.java
+++ Oppgave3/src/no/hvl/dat102/Binærsøking.java
@@ -1,16 +1,17 @@
 package no.hvl.dat102;
 
 public class Binærsøking {
-
-	public static int binærsøling(int[] tab, int tall) {
+	
+	public static int binærsøkling(int[] tab, int tall) {
 		int start = 0;
 		int slutt = tab.length - 1;
-
+		
 		while (start <= slutt) {
+			
 			int mid = (start + slutt) / 2;
 			if (tall == tab[mid]) {
 				return mid;
-			}else if(tall<tab[mid]) {
+			} else if (tall < tab[mid]) {
 				slutt = mid - 1;
 			} else {
 				start = mid + 1;
@@ -18,8 +19,9 @@ public class Binærsøking {
 		}
 		return -1;
 	}
-	public static void main (String[]args) {
-		int[] liste = { 2, 4, 5, 7, 8, 10, 12, 15, 18, 21, 23, 27, 29, 30, 31};
-		System.out.print(binærsøling(liste, 1));
+
+	public static void main(String[] args) {
+		int[] liste = { 2, 4, 5, 7, 8, 10, 12, 15, 18, 21, 23, 27, 29, 30, 31 };
+		System.out.println(binærsøkling(liste, 8));
 	}
 }
