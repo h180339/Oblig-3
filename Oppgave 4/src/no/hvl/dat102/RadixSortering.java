4ece9e8212234e010acfaa64251bc24d2e784a80
orjan <orjan@158-37-229-122.hib.no>
2018-03-19 10:23:26 +0100

    nonostream

--- Oppgave 4/src/no/hvl/dat102/RadixSortering.java
+++ Oppgave 4/src/no/hvl/dat102/RadixSortering.java
@@ -3,35 +3,37 @@ package no.hvl.dat102;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 public class RadixSortering {
 	public static void main(String[] args) {
-		int[] tabell = { 7843, 4568, 8765, 6543, 7865, 4532, 9987, 3241, 6589, 6622, 1211 };
+		int[] tabell = { 123, 398, 210, 19, 528, 513, 129, 294 };
 		final int RADIX = 10;
 		ArrayList<Integer> bøtteTabell[] = new ArrayList[RADIX];
-		for(int teller = 0; teller<bøtteTabell.length; teller++) {
+		for (int teller = 0; teller < bøtteTabell.length; teller++) {
 			bøtteTabell[teller] = new ArrayList<>();
 		}
+		// sorter inn i bøtetabell
 		boolean maxTallnåd = false;
 		int temp = -1, posisjon = 1;
-		while(!maxTallnåd) {
+		while (!maxTallnåd) {
 			maxTallnåd = true;
-			for(Integer element: tabell) {
+			for (Integer element : tabell) {
 				temp = element / posisjon;
 				bøtteTabell[temp % RADIX].add(element);
-				if(maxTallnåd && temp>0) {
+				if (maxTallnåd && temp > 0) {
 					maxTallnåd = false;
 				}
 			}
+
+			// plasserer tilbake i oprinnelig tabell
 			int a = 0;
-			for(int b = 0; b<RADIX; b++) {
-				for(Integer i:bøtteTabell[b]) {
+			for (int b = 0; b < RADIX; b++) {
+				for (Integer i : bøtteTabell[b]) {
 					tabell[a++] = i;
 				}
 				bøtteTabell[b].clear();
-			}		
+			}
 			posisjon = posisjon * RADIX;
 		}
-			Arrays.stream(tabell).forEach(System.out::println);
+		System.out.println("Listen etter fletting" + "\n" + Arrays.toString(tabell));
 	}
 }
